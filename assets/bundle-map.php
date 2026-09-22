c63a9a3a3d423958f93ec0a651784d423b67c6c3
Yamiru <vaskoviktor@gmail.com>
2026-09-22 17:47:54 +0200

    Add bundle-map.php for LiteBansU plugin assets

---
+++ assets/bundle-map.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * ============================================================================
+ * LiteBansU
+ * ============================================================================
+ *
+ * Plugin Name: LiteBansU
+ * Description: A modern, secure, and responsive web interface for LiteBans punishment management system.
+ * Version: 5.0
+ * Market URI: https://builtbybit.com/resources/litebansu-litebans-website.69448/
+ * Author URI: https://yamiru.com
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+ * ============================================================================
+ */
+return [
+    'css' => [
+        'css/variables.css',
+        'css/base.css',
+        'css/components.css',
+        'css/layout.css',
+        'css/themes.css',
+        'css/utilities.css',
+        'css/responsive.css',
+        'css/pages/home.css',
+        'css/pages/stats.css',
+        'css/pages/punishments.css',
+        'css/pages/detail.css',
+        'css/pages/admin.css',
+        'css/refresh.css',
+    ],
+    'js' => [
+        'js/main.js',
+    ],
+];
